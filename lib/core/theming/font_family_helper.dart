a5ba2b85dd189ced865fa2ac9dea68447eb04d88
OmarAmeer96 <omar.ameer244@gmail.com>
2024-12-29 22:57:06 +0200

    Remove unused regex, constants, shared preferences helper, and font family helper classes

--- lib/core/theming/font_family_helper.dart
+++
@@ -1,5 +0,0 @@
-class FontFamilyHelper {
-  static const String vazirmatnMedium = 'Vazirmatn-Medium';
-  static const String vazirmatnSemiBold = 'Vazirmatn-SemiBold';
-  static const String vazirmatnBold = 'Vazirmatn-Bold';
-}
--- lib/core/theming/styles.dart
+++ lib/core/theming/styles.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:lamerei_app/core/theming/font_family_helper.dart';
 
 class Styles {
   static TextStyle font11BottomNavMedium = TextStyle(
     color: Color(0xB2425486),
     fontSize: 11.sp,
-    fontFamily: FontFamilyHelper.vazirmatnMedium,
   );
 }
